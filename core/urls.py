b9c9ec2cd054c772883ca9911e15e5eaaa8bd1a1
Riley Weaver <c0wman1928@live.com>
2025-05-04 20:59:51 -0400

    Added a functional login page

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("champs/",     views.champ_list,   name="champ_list"),
+    path("",     views.champ_list,   name="champ_list"),
 
-    path("champs/<str:champ_key>/", views.champ_detail, name="champ_detail"),
+    path("<str:champ_key>/", views.champ_detail, name="champ_detail"),
 
 ]
